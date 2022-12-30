205c4f13af075149b105caac9db23a6ff605d7b6
Nika <nikaonime@gmail.com>
2022-12-30 14:53:38 +0200

    theory-start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import './Header' 
+import Header from './Header';
+import List from './List';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <List />
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function List() {
+    return (
+        <div>
+            <ul>
+                <li>meow</li>
+                <li>meow</li>
+                <li>meow</li>
+                <li>meow</li>
+            </ul>
+        </div>
+    )
+}
