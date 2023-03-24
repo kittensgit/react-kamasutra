1296beb71864f61b853c3cf89109429b0df06b06
Nika <nikaonime@gmail.com>
2023-03-24 11:09:19 +0200

    useState, useEffect

--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Preloader from '../../common/Preloader/Preloader';
-import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 function ProfileInfo(props) {
     if (!props.profile) {
         return <Preloader />
     }
-
-    debugger;
 
     return (
         <div>
@@ -15,7 +13,7 @@ function ProfileInfo(props) {
                 <img src='https://img.freepik.com/free-vector/vibrant-background-with-diagonal-lines-effect-design_1017-28226.jpg?w=996&t=st=1672660654~exp=1672661254~hmac=55cbc4ce2d93ba8fc3ff82adbe3fdde319ab7760d89dca9bcd138f0f06b2fa83' alt='img'/>
             </div> */}
             <img src={props.profile.photos.large} alt='ava'/>
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
         </div>
     )
 }
---
+++ src/Components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+
+const ProfileStatusWithHooks = (props) => {
+
+    const [editMode, setEditMode] = useState(false)
+    const [status, setStatus] = useState(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
+
+    const activateEditMode = () => {
+        setEditMode(true)
+    }
+
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status);
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
+    }
+
+    return (
+        <div>
+            { !editMode &&
+                <div>
+                    <span onDoubleClick={activateEditMode}>{props.status || 'no status'}</span>
+                </div>
+            }
+
+            {editMode &&
+                <div>
+                    <input onChange={onStatusChange} onBlur={deactivateEditMode} autoFocus={true} value={status}/>
+                </div>
+            }
+        </div>
+    )
+
+}
+
+export default ProfileStatusWithHooks;
